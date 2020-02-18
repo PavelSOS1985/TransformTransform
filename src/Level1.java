0877d8f8cb6c64493c50d151369ee121b35095ec
PavelSOS <sawel@yandex.ru>
2020-02-18 14:50:12 +0800

    Initial commit

---
+++ src/Level1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Level1 {
+    public static boolean TransformTransform(int[] A, int N) {
+        LinkedList<Integer> newA = new LinkedList<>();
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            newA.add(A[i]);
+        }
+        LinkedList<Integer> B = toB(newA, N);
+        LinkedList<Integer> B2 = toB(B, B.size());
+        for (Integer i :
+                B2) {
+            sum += i;
+        }
+        return sum % 2 == 0;
+    }
+
+    public static LinkedList<Integer> toB(LinkedList<Integer> A, int N) {
+        LinkedList<Integer> B = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                int k = i + j;
+                try {
+                    B.addLast(Collections.max(A.subList(j, k + 1)));
+                } catch (Exception e) {
+                }
+            }
+        }
+        return B;
+    }
+
+}
