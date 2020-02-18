f31f9a24a8362a0adfca69135ef36a5217b185e8
PavelSOS <sawel@yandex.ru>
2020-02-18 15:36:50 +0800

    ver 2

--- src/Level1.java
+++ src/Level1.java
@@ -25,6 +25,7 @@ public class Level1 {
                 try {
                     B.addLast(Collections.max(A.subList(j, k + 1)));
                 } catch (Exception e) {
+
                 }
             }
         }
